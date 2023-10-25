4bede6d38131c10f20d0f26e43fa083d04fb3a66
Nitika- Thakur123 <thakurnitika474@gmail.com>
2023-10-25 17:27:31 +0530

    new changes

---
+++ day1/Introduction_to_JS.js
@@ -0,0 +1,50 @@
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+console.log("Masai School");
+console.log("A Transformation in Education");
+
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name = "Nitika Thakur"
+let fname = "Shriram Thakur"
+let mname = "Sarla Thakur"
+console.log(mname);
+
+
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name1 ="Nitika Thakur"
+console.log(type(name1),name1);
+let age=25;
+console.log(type(age),age);
+
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art 
+// (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+let name2 =`ℕ𝕚𝕥𝕚𝕜𝕒`;
+let school =`🇲​​​​​🇦​​​​​🇸​​​​​🇦​​​​​🇮​​​​​`;
+let grade ="A";
+let section =`🇨​​​​​🇦​​​​​🇵​​​​​06`;
+let roll_no =198;
+let Math =80;
+let science =82;
+let english =83;
+console.log("***********************Report Card*******************");
+console.log("**","Name:",name2,"                                   ")                                  
+console.log("**","school:",school,"                               ")                                   
+console.log("**","grade:",grade,"                                 ")                                
+console.log("**","section:",section,"                             ")
+console.log("**","roll_no:",roll_no,"                             ")
+console.log("**","Math:",Math,"                                   ")
+console.log("**","science:",science,"                             ")
+console.log("**","english:",english,"                             ")
+console.log("*****************************************************");
